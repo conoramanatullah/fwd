9efb4dfaa53d32c8eac90c7b2597ea5cb4ec0edd
Conor <conor.amanatullah@colorado.edu>
2016-10-27 09:47:10 -0600

    simplified lab 12

--- js/lab12.js
+++ js/lab12.js
@@ -12,9 +12,7 @@
                   'http://i1.kym-cdn.com/photos/images/facebook/000/632/613/42d.jpg'
                   ];
     var index = 0;
-    var sepia = false;
-    var invert = false;
-    var hue = false;
+
     $('#slideshow').on('click', function(){
       var image = $('#slideshow #image img');
       if(index == images.length - 1){
@@ -28,33 +26,19 @@
     });
     $('#button1').on('click', function(){
       console.log("button 1 clicked");
-      if(sepia){
-        $('#image img').removeClass('sepia');
-        sepia = false;
-      } else {
-        $('#image img').addClass('sepia');
-        sepia = true;
-      }
+
+      $('#image img').toggleClass('sepia');
+
+
     });
     $('#button2').on('click', function(){
-      console.log("button 2 clicked");
-      if(invert){
-        $('#image img').removeClass('invert');
-        invert = false;
-      } else {
-        $('#image img').addClass('invert');
-        invert = true;
-      }
+      $('#image img').toggleClass('invert');
+
     });
     $('#button3').on('click', function(){
       console.log("button 3 clicked");
-      if(hue){
-        $('#image img').removeClass('hue');
-        hue = false;
-      } else {
-        $('#image img').addClass('hue');
-        hue = true;
-      }
+      $('#image img').toggleClass('hue');
+
     });
 
   });
